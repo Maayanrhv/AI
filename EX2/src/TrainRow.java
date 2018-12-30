72246d1f0a096c4d397a456cbe70f3ecbb306469
Maayan <maayanrhv@gmail.com>
2018-12-30 23:32:37 +0200

    adding trainRows class

--- EX2/src/TrainRow.java
+++ EX2/src/TrainRow.java
@@ -1,3 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrainRow {
+    /* Properties */
+    Map<String,String> values = new HashMap<>();
+    int rowNumber;
+    String classification;
+
+    /* Constructors */
+
+    /* Getters & Setters */
+    /**
+     * @return values
+     */
+    public Map<String, String> getValues() {
+        return values;
+    }
+    /**
+     * @return rowNumber
+     */
+    public int getRowNumber() {
+        return rowNumber;
+    }
+    /**
+     * @return classification
+     */
+    public String getClassification() {
+        return classification;
+    }
+
+    /**
+     * @param valuesVal to set as values
+     */
+    public void setValues(Map<String, String> valuesVal) {
+        this.values = valuesVal;
+    }
+    /**
+     * @param rowNumberVal to set as rowNumber
+     */
+    public void setRowNumber(int rowNumberVal) {
+        this.rowNumber = rowNumberVal;
+    }
+    /**
+     * @param classificationVal to set as classification
+     */
+    public void setClassification(String classificationVal) {
+        this.classification = classificationVal;
+    }
+
+    /* Methods */
 
 }
