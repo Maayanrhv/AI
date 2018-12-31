dc006d8636d6d2c0c8f457f8b63910c4dcc04f74
Maayan <maayanrhv@gmail.com>
2018-12-31 12:59:42 +0200

    Added Data object properly

---
+++ EX2/src/Data.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class holds the training data according to which,
+ * each algorithm will calculate the classification for each test
+ * in the test file.
+ */
+public class Data {
+    /* Properties */
+    private ArrayList<TrainRow> trainRows = new ArrayList<>();
+    private ArrayList<String>[] possibleAttributes;
+    private Map<Integer, String> attributesPositions = new HashMap<>();
+    private ArrayList<String> possibleClassifications = new ArrayList<>();
+    private String classificationName;
+    private int classificationPosition;
+    private int amountOfAttributes;
+
+    /* Getters & Setters */
+    /**
+     * @return trainRows
+     */
+    public ArrayList<TrainRow> getTrainRows() {
+        return trainRows;
+    }
+    /**
+     * @return possibleAttributes
+     */
+    public ArrayList<String>[] getPossibleAttributes() {
+        return possibleAttributes;
+    }
+    /**
+     * @return attributesPositions
+     */
+    public Map<Integer, String> getAttributesPositions() {
+        return attributesPositions;
+    }
+    /**
+     * @return classificationName
+     */
+    public String getClassificationName(){
+        return classificationName;
+    }
+    /**
+     * @return possibleClassifications
+     */
+    public ArrayList<String> getPossibleClassifications() {
+        return possibleClassifications;
+    }
+    /**
+     * @return classificationPosition
+     */
+    public int getClassificationPosition() {
+        return classificationPosition;
+    }
+    /**
+     * @return amountOfAttributes
+     */
+    public int getAmountOfAttributes() {
+        return amountOfAttributes;
+    }
+
+    /**
+     * @param trainRowsVal to set as trainRows
+     */
+    public void setTrainRows(ArrayList<TrainRow> trainRowsVal) {
+        this.trainRows = trainRowsVal;
+    }
+    /**
+     * @param possibleAttributesVal to set as possibleAttributes
+     */
+    public void setPossibleAttributes(ArrayList<String>[] possibleAttributesVal) {
+        this.possibleAttributes = possibleAttributesVal;
+    }
+    /**
+     * @param attributesPositionsVal to set as attributesPositions
+     */
+    public void setAttributesPositions(Map<Integer, String> attributesPositionsVal) {
+        this.attributesPositions = attributesPositionsVal;
+    }
+    /**
+     * @param classificationNameVal to set as classificationName
+     */
+    public void setClassificationName(String classificationNameVal) {
+        this.classificationName = classificationNameVal;
+    }
+    /**
+     * @param possibleClassificationsVal to set as possibleClassifications
+     */
+    public void setPossibleClassifications(ArrayList<String> possibleClassificationsVal) {
+        this.possibleClassifications = possibleClassificationsVal;
+    }
+    /**
+     * @param classificationPositionVal to set as classificationPosition
+     */
+    public void setClassificationPosition(int classificationPositionVal) {
+        this.classificationPosition = classificationPositionVal;
+    }
+    /**
+     * @param amountOfAttributesVal to set as amountOfAttributes
+     */
+    public void setAmountOfAttributes(int amountOfAttributesVal) {
+        amountOfAttributes = amountOfAttributesVal;
+    }
+
+
+    /* Methods */
+}
